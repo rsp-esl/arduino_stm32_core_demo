4bef2c78283e3a462da41e317421bfc3d8cb73f0
rsp <siripokarpirom@gmail.com>
2017-12-30 12:42:38 +0700

    Add files via upload

--- stm32_f401re_analog_read.ino
+++ stm32_f401re_analog_read.ino
@@ -5,12 +5,10 @@
 // Author: Rawat S.
 // Date: 2017-12-28
 ////////////////////////////////////////////////////////////////////////////
-
-#define AIN_PIN       PA0  // PA_0 or A0 pin (input voltage: 0V .. 3.3V only)
+#define AIN_PIN       A0  // PA_0 or A0 pin (input voltage: 0V .. 3.3V only)
 #define DELAY_MS      500
 
-char sbuf[32];
-String str;
+char sbuf[64];
 
 void setup() {
   Serial.begin( 115200 );
@@ -19,14 +17,12 @@ void setup() {
 
 void loop() {
   int value = analogRead( AIN_PIN ); // value between 0..4095
-  dtostrf( 3.3*value/4095, 4, 3, sbuf );
-  str = "Reading: ";
-  str += value;
-  str += ", ";
-  str += sbuf;
-  str += " V";
-  Serial.println( str.c_str() );
+  float volt = 3.3*value/4095;
+  sprintf( sbuf, "Reading A%d: %4d, %d.%03d V",
+           (AIN_PIN-A0), value, int(volt), int(volt*1000)%1000 );
+  Serial.println( sbuf );
   delay( DELAY_MS );
 }
 ////////////////////////////////////////////////////////////////////////////
+
 
