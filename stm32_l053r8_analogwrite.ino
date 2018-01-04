45d1a5576e15590f5020134f41cdf02fa7d72350
rsp <siripokarpirom@gmail.com>
2018-01-04 19:25:58 +0700

    Add files via upload

---
+++ stm32_l053r8_analogwrite.ino
@@ -0,0 +1,44 @@
+
+#define DAC_PIN   A2 // for DAC1/1 on Nucleo L053R8
+// see: https://os.mbed.com/platforms/ST-Nucleo-L053R8/
+
+int value = 0;
+char sbuf[64];
+int pm = 1;
+
+void setup() {
+  Serial.begin( 115200 );
+  Serial.print( F("\n\n\n") );
+  sprintf( sbuf, "System clock frequency %d MHz\n", SystemCoreClock/1000000 );
+  Serial.print( sbuf );
+  Serial.flush();
+  
+  pinMode( DAC_PIN, OUTPUT );
+  
+#ifndef HAL_DAC_MODULE_ENABLED
+  Serial.println( "DAC is not supported !!!" );
+#endif
+
+  analogWriteResolution( 12 );
+  analogWrite( DAC_PIN, value );
+  
+}
+
+#define VALUE_MAX  4095
+#define VALUE_MIN  0
+
+void loop() {
+  value += pm*10;
+  if ( value > VALUE_MAX ) {
+    value = VALUE_MAX; 
+  } else if ( value < VALUE_MIN ) { 
+    value = VALUE_MIN; 
+  }
+  analogWrite( DAC_PIN, value );
+  if ( value >= VALUE_MAX && pm == 1 ){
+     pm = -1;
+  }  else if ( value <= VALUE_MIN && pm == -1 ){
+     pm = 1;
+  }
+  delay(5);
+}
